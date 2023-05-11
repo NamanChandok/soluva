46bcdaf1b65ec86fb54e1f8096394bb00413789f
Naman <namanchandok1@gmail.com>
2023-05-11 10:45:37 +0530

    r

--- app/portfolio/worksdata.js
+++ app/portfolio/worksdata.js
@@ -1,27 +1,27 @@
 export const worksData = [
     {
         title: "R.D.Physiotherapy Clinic",
-        imgUrl: "./projects/rdphysiotherapy.png",
+        imgUrl: "/projects/rdphysiotherapy.png",
         url: "http://rdphysiotherapyclinic.co.in/"
     },
     {
         title: "1up webhost",
-        imgUrl: "./projects/1upwebhost.png",
+        imgUrl: "/projects/1upwebhost.png",
         url: "https://1upwebhost.com/"
     },
     {
         title: "Findmyinfluence",
-        imgUrl: "./projects/findmyinfluence.png",
+        imgUrl: "/projects/findmyinfluence.png",
         url: "https://findmyinfluence.com/"
     },
     {
         title: "Hearts Marriage",
-        imgUrl: "./projects/heartsmarriage.png",
+        imgUrl: "/projects/heartsmarriage.png",
         url: "https://heartsmarriage.com/"
     },
     {
         title: "AiCore",
-        imgUrl: "./projects/aicore.png",
+        imgUrl: "/projects/aicore.png",
         url: "https://aicore.us/"
     }
 ]
